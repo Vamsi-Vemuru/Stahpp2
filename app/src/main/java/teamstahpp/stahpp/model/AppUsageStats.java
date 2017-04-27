00e221b6256331053bec60e1de4673f6d5b58f1b
vamsi <vamsi0493@gmail.com>
2017-04-26 21:03:41 -0400

    notifications code in main2activity. Added settings fragment. MainActivity redundant now.

---
+++ app/src/main/java/teamstahpp/stahpp/model/AppUsageStats.java
@@ -0,0 +1,13 @@
+package teamstahpp.stahpp.model;
+
+import android.app.usage.UsageStats;
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by ISR on 12-Apr-17.
+ */
+
+public class AppUsageStats {
+    public UsageStats usageStats;
+    public Drawable appIcon;
+}
